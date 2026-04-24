1a509419fc3e7b1f886a0f7e381d86669a08f869
yeopseung <108778451+YeopSeung04@users.noreply.github.com>
2026-04-24 09:42:58 +0900

    Blueprint camera toggle, boss HP UI, and packaging fixes
    - Added V key camera toggle in Blueprint
    - Switched between 1st-person and 3rd-person camera views
    - Hooked up boss HP bar UI updates
    - Fixed boss death flow to trigger game end
    - Added packaged-build map include settings
    - Updated gameplay Blueprint flow and input handling

--- Source/Interior/Private/Interior.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Yeop. All Rights Reserved.
-
-#include "Interior.h"
-#include "Modules/ModuleManager.h"
-
-DEFINE_LOG_CATEGORY(LogInterior);
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Interior, "Interior");
--- Source/Interior/Public/Interior.h
+++
@@ -1,8 +0,0 @@
-// Copyright Yeop. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Modules/ModuleManager.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(LogInterior, Log, All);
